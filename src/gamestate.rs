2415a88a6588c8fb2bb0984bb2887f111e685ceb
Nick Spain <nicholas.spain96@gmail.com>
2020-02-15 13:38:39 +1100

    Use a black square for testing

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -9,7 +9,7 @@ pub struct GameState;
 impl SimpleState for GameState {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
-        let sprite_sheet_handle = load_sprite_sheet(world, "textures/skeleton_spritesheet");
+        let sprite_sheet_handle = load_sprite_sheet(world, "textures/black_square");
         world.register::<Player>();
         init_player(world, sprite_sheet_handle.clone());
         init_camera(world);
